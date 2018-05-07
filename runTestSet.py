79190f8fd13f8c6d0cd4f935a88b314b2a6039c6
riveSunder <thescinder@gmail.com>
2018-05-07 22:14:49 +0100

    add wrapper for running agent containers through test levels

--- runTestSet.py
+++ runTestSet.py
@@ -5,26 +5,36 @@ import subprocess
 import os
 import numpy as np
 
+# imports for reading in the agent name
+import sys, getopt
+
 # Where the list of games and states
 testSetList = './data/sonic-test.csv'
-agent = 'jerk-agent:v1'
+
 # run time (based on system clock)
 maxWallTime = 1200
 
+# Setup the agent
+opts, args = getopt.getopt(sys.argv[1:],"a:",["agent="])
+agent = 'jerk-agent:v1'
 
+for opt, arg in opts:
+	if opt in ("-a","--agent"):
+		agent = arg
+print(agent)
 
 gameList = pd.read_csv(testSetList)
 lenList = len(gameList)
-
-for ck in range(lenList):
-	# save results in appropriate folder
-	resultsDir = "./results/" + agent[0:4] +"/" + gameList.game[ck] + gameList.state[ck]
-	# 
-	bashCommand = "retro-contest run --agent $DOCKER_REGISTRY/" + agent + " --results-dir " + resultsDir + " --no-nv --use-host-data --wallclock-limit " + str(maxWallTime) + " " + gameList.game[ck] + " " + gameList.state[ck]
-	print(bashCommand)
-	subprocess.call(bashCommand,shell=True)
-	#process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, shell=True)
-	#output, error = process.communicate()
+if(1):
+	for ck in range(9,lenList):
+		# save results in appropriate folder
+		resultsDir = "./results/" + agent[0:4] +"2/" + gameList.game[ck] + gameList.state[ck]
+		# 
+		bashCommand = "retro-contest run --agent $DOCKER_REGISTRY/" + agent + " --results-dir " + resultsDir + " --no-nv --use-host-data --wallclock-limit " + str(maxWallTime) + " " + gameList.game[ck] + " " + gameList.state[ck]
+		print(bashCommand)
+		subprocess.call(bashCommand,shell=True)
+		#process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, shell=True)
+		#output, error = process.communicate()
 
 
 myCurDir = os.listdir("./results/" + agent[0:4]+"/")
@@ -32,8 +42,8 @@ myTestScores = []
 outFile = open("./results/"+ agent[0:4] + "/summary.txt","w")
 
 for myFolder in myCurDir:
-	print("./results/"+ agent[0:4] + myFolder)
-	myScores = pd.read_csv("./results/"+ agent[0:4] + "/" + myFolder)
+	print("./results/"+ agent[0:4] + myFolder+"/monitor.csv")
+	myScores = pd.read_csv("./results/"+ agent[0:4] + "/" + myFolder+"/monitor.csv")
 
 	# The score is based on the rewards during last n episodes
 	n = 10 
@@ -43,8 +53,8 @@ for myFolder in myCurDir:
 	myTestScores.append(avgScore)
 
 	# Write out text file with results
-	text_file.write("Score for %s = %.2f +/- %.2f" % (myFolder,avgScore,stdScore))
-
+	outFile.write("Score for %s = %.2f +/- %.2f" % (myFolder,avgScore,stdScore))
+outFile.write("Score for Aggregate = %.2f +/- %.2f" % (np.mean(myTestScores),np.std(myTestScores)))
 print("avg test scores", myTestScores)
 outFile.close()
 
